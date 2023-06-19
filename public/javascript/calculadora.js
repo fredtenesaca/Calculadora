893c080a3d2bfa453725b9ca36581043a6142194
Freddy <fregtene@espol.edu.ec>
2023-06-19 03:01:21 -0500

    calcular

--- public/javascript/calculadora.js
+++ public/javascript/calculadora.js
@@ -1,4 +1,4 @@
-const funcionPredeterminada = () => {
+function funcionPredeterminada() {
     const formulario = document.getElementById('formulario');
 
     formulario.addEventListener('submit', (evento) => {
@@ -7,21 +7,30 @@ const funcionPredeterminada = () => {
         const valorPorHoraInput = document.getElementById('valorPorHora');
         const horas1Input = document.getElementById('horas1');
         const horas2Input = document.getElementById('horas2');
+
+        const horas3Input = document.getElementById('horas3');
+        const horas4Input = document.getElementById('horas4');
+
+
+
         const valorTotalElemento = document.getElementById('valorTotal');
 
         const valorPorHora = parseFloat(valorPorHoraInput.value);
         const horas1 = parseFloat(horas1Input.value);
         const horas2 = parseFloat(horas2Input.value);
 
-        const valorTotal = valorPorHora * (horas1 + horas2);
+        const horas3 = parseFloat(horas3Input.value);
+        const horas4 = parseFloat(horas4Input.value);
+
+        const valorTotal = valorPorHora * (horas1 + horas2 + horas3 + horas4);
 
         valorTotalElemento.textContent = valorTotal.toFixed(2);
- 
- 
-      });
 
-    
-  };
+
+    });
+
+
+}
   
   // Invocar la función predeterminada
   funcionPredeterminada();
